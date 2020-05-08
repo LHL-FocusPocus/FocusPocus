796c16f1f514e78961a2e313000e0345f59e6b28
xynyx <matt.taylr22@gmail.com>
2020-05-08 18:29:14 -0400

    Sign-in template; added image above

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SignUp from './SignUp'
+import styled from 'styled-components'
+
+
+const Page = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+
+export default function Landing() {
+  return (
+    <Page>
+      <SignUp/>
+    </Page>
+  )
+}
