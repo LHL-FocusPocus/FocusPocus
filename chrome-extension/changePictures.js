9e715c3a1e01d0840d869da4d6a7bae4f1820412
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-08 19:00:49 -0400

    refactor to replaceImages fn, wait for readystate

--- chrome-extension/changePictures.js
+++ chrome-extension/changePictures.js
@@ -1,21 +1,36 @@
-// Replace images specified by img tags
-const imgTagElements = document.querySelectorAll("img");
-for (const imgTagElement of imgTagElements) {
-  imgTagElement.setAttribute(
-    "src",
-    "https://memegen.link/bad/get_back_to_work/lazy_bum.jpg"
-  );
-  imgTagElement.setAttribute(
-    "data-src",
-    "https://memegen.link/bad/get_back_to_work/lazy_bum.jpg"
-  );
-}
+{
+  const replaceImages = function () {
+    // Replace images specified by img tags
+    const imgTagElements = document.querySelectorAll("img");
+    for (const imgTagElement of imgTagElements) {
+      imgTagElement.setAttribute(
+        "src",
+        "https://memegen.link/bad/get_back_to_work/lazy_bum.jpg"
+      );
+      imgTagElement.setAttribute(
+        "data-src",
+        "https://memegen.link/bad/get_back_to_work/lazy_bum.jpg"
+      );
+    }
 
-// Replace images specified by background-image css
-const backgroundImageElements = document.querySelectorAll(
-  '[style*="background-image"]'
-);
-for (const bgImageElement of backgroundImageElements) {
-  bgImageElement.style.backgroundImage =
-    'url("https://memegen.link/bad/get_back_to_work/lazy_bum.jpg")';
+    // Replace images specified by background-image css
+    const backgroundImageElements = document.querySelectorAll(
+      // *= matches all style attribute containing "background-image" in its value
+      '[style*="background-image"]'
+    );
+    for (const bgImageElement of backgroundImageElements) {
+      bgImageElement.style.backgroundImage =
+        'url("https://memegen.link/bad/get_back_to_work/lazy_bum.jpg")';
+    }
+  };
+
+  // Replacement of jQuery's document.ready
+  function ready(fn) {
+    if (document.readyState != "loading") {
+      fn();
+    } else {
+      document.addEventListener("DOMContentLoaded", fn);
+    }
+  }
+  ready(replaceImages);
 }
