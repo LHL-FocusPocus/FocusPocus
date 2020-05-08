12966ad5d1712793c4d7788fe821282e9b97ebf1
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-08 18:45:46 -0400

    replaces background-image style tag images

--- chrome-extension/changePictures.js
+++ chrome-extension/changePictures.js
@@ -1,1 +1,21 @@
-document.body.style.backgroundColor = "orange"
+// Replace images specified by img tags
+const imgTagElements = document.querySelectorAll("img");
+for (const imgTagElement of imgTagElements) {
+  imgTagElement.setAttribute(
+    "src",
+    "https://memegen.link/bad/get_back_to_work/lazy_bum.jpg"
+  );
+  imgTagElement.setAttribute(
+    "data-src",
+    "https://memegen.link/bad/get_back_to_work/lazy_bum.jpg"
+  );
+}
+
+// Replace images specified by background-image css
+const backgroundImageElements = document.querySelectorAll(
+  '[style*="background-image"]'
+);
+for (const bgImageElement of backgroundImageElements) {
+  bgImageElement.style.backgroundImage =
+    'url("https://memegen.link/bad/get_back_to_work/lazy_bum.jpg")';
+}
