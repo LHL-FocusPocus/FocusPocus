aed320fb6341bf3fb6728a672d56fe422a905a64
Taha Rizvi <riztaha@gmail.com>
2020-05-09 15:18:17 -0400

    Fixed JSON to JSONB

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -8,5 +8,5 @@ CREATE TABLE users (
 
   email      VARCHAR(255) UNIQUE NOT NULL,
   password   VARCHAR(255) NOT NULL,
-  options    JSON
+  options    JSONB
 );
--- db/schema/03_blacklists.sql
+++ db/schema/03_blacklists.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS blacklists CASCADE;
 
 CREATE TABLE blacklists
 (
-  id SERIAL PRIMARY KEY,
+  id          SERIAL PRIMARY KEY,
 
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  website_id INTEGER REFERENCES websites(id) ON DELETE CASCADE
+  user_id     INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  website_id  INTEGER REFERENCES websites(id) ON DELETE CASCADE
 )
