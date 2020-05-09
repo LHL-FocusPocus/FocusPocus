d5559af1f20827b1ecf3369080732cb7bd8383f6
Taha Rizvi <riztaha@gmail.com>
2020-05-09 02:07:22 -0400

    Added seed data for tables

--- server/db/schema/05_quotas.sql
+++ server/db/schema/05_quotas.sql
@@ -11,4 +11,5 @@ CREATE TABLE quotas
   date_valid_until TIMESTAMP WITHOUT TIME ZONE + INTERVAL '1 day'
 )
 -- Not sure what values the dates should hold. Should it be the current date/time when the quota was created?
--- Should valid_until just be 24 hrs from when the quota was created?
+-- Should valid_until just be 24 hrs from when the quota was created? Should it be the timestamp + interval? <-- Was having trouble attempting this
+-- If we have interval, then we technically don't need date_valid_until in the db.
---
+++ server/db/seeds/04_browse_times.sql
@@ -0,0 +1,17 @@
+INSERT INTO browse_times
+  (user_id, website_hostname, datetime_startdate, duration)
+VALUES
+  (1, 'www.reddit.com', '2020-05-09 01:23:45+02', 600);
+INSERT INTO browse_times
+  (user_id, website_hostname, datetime_startdate, duration)
+VALUES
+  (1, 'www.cnn.com', '2020-05-09 01:33:45+02', 600);
+
+INSERT INTO browse_times
+  (user_id, website_hostname, datetime_startdate, duration)
+VALUES
+  (2, 'www.espn.com', '2020-05-09 01:23:45+02', 600);
+INSERT INTO browse_times
+  (user_id, website_hostname, datetime_startdate, duration)
+VALUES
+  (2, 'www.facebook.com', '2020-05-09 01:33:45+02', 600);
