5609fd65ab1fa946aaa22885dd39c80b1a51d6b2
Taha Rizvi <riztaha@gmail.com>
2020-05-09 22:33:41 -0400

    Added getTotalTimeByUserID query

--- db/seeds/04_browse_times.sql
+++ db/seeds/04_browse_times.sql
@@ -1,17 +1,17 @@
 INSERT INTO browse_times
   (user_id, website_id, datetime_start, duration)
 VALUES
-  (1, 1, '2020-05-09 01:23:45+02', '10 minutes');
+  (1, 1, '2020-05-09 01:23:45-04', '10 minutes');
 INSERT INTO browse_times
   (user_id, website_id, datetime_start, duration)
 VALUES
-  (1, 3, '2020-05-09 01:33:45+02', '10 minutes');
+  (1, 3, '2020-05-09 15:23:45-04', '10 minutes');
 
 INSERT INTO browse_times
   (user_id, website_id, datetime_start, duration)
 VALUES
-  (2, 4, '2020-05-09 01:23:45+02', '10 minutes');
+  (2, 4, '2020-05-09 14:23:45-04', '10 minutes');
 INSERT INTO browse_times
   (user_id, website_id, datetime_start, duration)
 VALUES
-  (2, 2, '2020-05-09 01:33:45+02', '10 minutes');
+  (2, 2, '2020-05-09 12:23:45-04', '10 minutes');
