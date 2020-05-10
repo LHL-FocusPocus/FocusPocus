0243f09b239851f964276f09707e9ae195e16513
xynyx <matt.taylr22@gmail.com>
2020-05-10 17:39:45 -0400

    Create flex container to hold charts

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import Navbar from "./Navbar"
+import styled from "styled-components";
+import DailyQuotaUsed from "./DailyQuotaUsed";
+import Box from '@material-ui/core/Box';
+
+
+import { flexbox, flex, flexGrow } from '@material-ui/system';
+
+
+const Container = styled(Box)`
+  ${'' /* display: flex; */}
+  flex-flow: row-wrap;
+  margin: 4em;
+`
+
+const Background = styled.div`
+  background-image: none;
+`
 
 export default function Dashboard() {
   return (
-    <div>
+    <Background>
       <Navbar />
-    </div>
+      <Container flexWrap="wrap" width="100%" display="flex">
+        <DailyQuotaUsed flexGrow={1} />
+      </Container>
+    </Background>
   )
 }
