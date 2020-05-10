021272374829e3bf98c594c42fcb95ff2d95f24b
Taha Rizvi <riztaha@gmail.com>
2020-05-09 23:42:28 -0400

    Added getTotalBlacklistTimeForTodayByUserID

--- server/db/seeds/04_browse_times.sql
+++ server/db/seeds/04_browse_times.sql
@@ -1,11 +1,16 @@
 INSERT INTO browse_times
   (user_id, website_id, datetime_start, duration)
 VALUES
-  (1, 1, '2020-05-09 01:23:45-04', '10 minutes');
+  (1, 0, '2020-05-09 01:23:45-04', '45 minutes');
+
 INSERT INTO browse_times
   (user_id, website_id, datetime_start, duration)
 VALUES
-  (1, 3, '2020-05-09 15:23:45-04', '10 minutes');
+  (1, 1, '2020-05-09 01:23:45-04', '29 minutes');
+INSERT INTO browse_times
+  (user_id, website_id, datetime_start, duration)
+VALUES
+  (1, 3, '2020-05-09 15:23:45-04', '13 minutes');
   INSERT INTO browse_times
   (user_id, website_id, datetime_start, duration)
 VALUES
@@ -23,3 +28,7 @@ INSERT INTO browse_times
   (user_id, website_id, datetime_start, duration)
 VALUES
   (2, 2, '2020-05-09 12:23:45-04', '10 minutes');
+  INSERT INTO browse_times
+  (user_id, website_id, datetime_start, duration)
+VALUES
+  (2, 0, '2020-05-09 01:23:45-04', '150 minutes');
