f2c36c067bf9af6e5cc29d55330e75f1ac5b62a2
xynyx <matt.taylr22@gmail.com>
2020-05-10 12:10:23 -0400

    Remove extraneous comment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,8 +9,6 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-// req cookie sessions
-
 module.exports = (db) => {
   const dbHelper = require("../helpers/dbHelper")(db);
   router.post("/login", (req, res) => {
