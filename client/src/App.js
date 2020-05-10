b6159b507058d6c09a76ff8e20700b9179aa47bd
xynyx <matt.taylr22@gmail.com>
2020-05-10 12:48:04 -0400

    Initialize dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Landing from "./components/Landing";
 import LogoText from "./components/LogoText";
 import styled from "styled-components";
+import Dashboard from "./components/Dashboard";
+import { Route, Link } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <Landing />
+        <Route exact path={["/", "/register"]} component={Landing} />
+        <Route exact path="dashboard" component={Dashboard} />
     </div>
   );
 }
