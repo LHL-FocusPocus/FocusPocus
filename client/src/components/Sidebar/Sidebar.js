273fcdf4a0485f6c0039861c9d31e450248e3dd2
xynyx <matt.taylr22@gmail.com>
2020-05-10 13:47:23 -0400

    Abandoning experiment

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -12,10 +12,10 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Icon from "@material-ui/core/Icon";
 // core components
-// import AdminNavbarLinks from "components/Navbars/AdminNavbarLinks.js";
-// import RTLNavbarLinks from "components/Navbars/RTLNavbarLinks.js";
+import AdminNavbarLinks from "components/Navbars/AdminNavbarLinks.js";
+import RTLNavbarLinks from "components/Navbars/RTLNavbarLinks.js";
 
-import styles from "../assets/jss/material-dashboard-react/components/sidebarStyle.js";
+import styles from "assets/jss/material-dashboard-react/components/sidebarStyle.js";
 
 const useStyles = makeStyles(styles);
 
@@ -25,63 +25,63 @@ export default function Sidebar(props) {
   function activeRoute(routeName) {
     return window.location.href.indexOf(routeName) > -1 ? true : false;
   }
-  // const { color, logo, image, logoText, routes } = props;
-  // var links = (
-  //   <List className={classes.list}>
-  //     {routes.map((prop, key) => {
-  //       var activePro = " ";
-  //       var listItemClasses;
-  //       if (prop.path === "/upgrade-to-pro") {
-  //         activePro = classes.activePro + " ";
-  //         listItemClasses = classNames({
-  //           [" " + classes[color]]: true
-  //         });
-  //       } else {
-  //         listItemClasses = classNames({
-  //           [" " + classes[color]]: activeRoute(prop.layout + prop.path)
-  //         });
-  //       }
-  //       const whiteFontClasses = classNames({
-  //         [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
-  //       });
-  //       return (
-  //         <NavLink
-  //           to={prop.layout + prop.path}
-  //           className={activePro + classes.item}
-  //           activeClassName="active"
-  //           key={key}
-  //         >
-  //           <ListItem button className={classes.itemLink + listItemClasses}>
-  //             {typeof prop.icon === "string" ? (
-  //               <Icon
-  //                 className={classNames(classes.itemIcon, whiteFontClasses, {
-  //                   [classes.itemIconRTL]: props.rtlActive
-  //                 })}
-  //               >
-  //                 {prop.icon}
-  //               </Icon>
-  //             ) : (
-  //               <prop.icon
-  //                 className={classNames(classes.itemIcon, whiteFontClasses, {
-  //                   [classes.itemIconRTL]: props.rtlActive
-  //                 })}
-  //               />
-  //             )}
-  //             <ListItemText
-  //               primary={props.rtlActive ? prop.rtlName : prop.name}
-  //               className={classNames(classes.itemText, whiteFontClasses, {
-  //                 [classes.itemTextRTL]: props.rtlActive
-  //               })}
-  //               disableTypography={true}
-  //             />
-  //           </ListItem>
-  //         </NavLink>
-  //       );
-  //     })}
-  //   </List>
-  
+  const { color, logo, image, logoText, routes } = props;
+  var links = (
+    <List className={classes.list}>
+      {routes.map((prop, key) => {
+        var activePro = " ";
+        var listItemClasses;
+        if (prop.path === "/upgrade-to-pro") {
+          activePro = classes.activePro + " ";
+          listItemClasses = classNames({
+            [" " + classes[color]]: true
+          });
+        } else {
+          listItemClasses = classNames({
+            [" " + classes[color]]: activeRoute(prop.layout + prop.path)
+          });
+        }
+        const whiteFontClasses = classNames({
+          [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
+        });
+        return (
+          <NavLink
+            to={prop.layout + prop.path}
+            className={activePro + classes.item}
+            activeClassName="active"
+            key={key}
+          >
+            <ListItem button className={classes.itemLink + listItemClasses}>
+              {typeof prop.icon === "string" ? (
+                <Icon
+                  className={classNames(classes.itemIcon, whiteFontClasses, {
+                    [classes.itemIconRTL]: props.rtlActive
+                  })}
+                >
+                  {prop.icon}
+                </Icon>
+              ) : (
+                <prop.icon
+                  className={classNames(classes.itemIcon, whiteFontClasses, {
+                    [classes.itemIconRTL]: props.rtlActive
+                  })}
+                />
+              )}
+              <ListItemText
+                primary={props.rtlActive ? prop.rtlName : prop.name}
+                className={classNames(classes.itemText, whiteFontClasses, {
+                  [classes.itemTextRTL]: props.rtlActive
+                })}
+                disableTypography={true}
+              />
+            </ListItem>
+          </NavLink>
+        );
+      })}
+    </List>
+  );
   var brand = (
-    <div className>
+    <div className={classes.logo}>
       <a
         href="https://www.creative-tim.com?ref=mdr-sidebar"
         className={classNames(classes.logoLink, {
@@ -89,10 +89,10 @@ export default function Sidebar(props) {
         })}
         target="_blank"
       >
-        <div className>
-          <img alt="logo" className={classes.img} />
+        <div className={classes.logoImage}>
+          <img src={logo} alt="logo" className={classes.img} />
         </div>
-
+        {logoText}
       </a>
     </div>
   );
@@ -115,15 +115,15 @@ export default function Sidebar(props) {
         >
           {brand}
           <div className={classes.sidebarWrapper}>
-            {/* {props.rtlActive ? <RTLNavbarLinks /> : <AdminNavbarLinks />} */}
-            {/* {links} */}
+            {props.rtlActive ? <RTLNavbarLinks /> : <AdminNavbarLinks />}
+            {links}
           </div>
-          {/* {image !== undefined ? (
+          {image !== undefined ? (
             <div
               className={classes.background}
               style={{ backgroundImage: "url(" + image + ")" }}
             />
-          ) : null} */}
+          ) : null}
         </Drawer>
       </Hidden>
       <Hidden smDown implementation="css">
@@ -138,13 +138,13 @@ export default function Sidebar(props) {
           }}
         >
           {brand}
-          <div className={classes.sidebarWrapper}></div>
-          {/* {image !== undefined ? (
+          <div className={classes.sidebarWrapper}>{links}</div>
+          {image !== undefined ? (
             <div
               className={classes.background}
               style={{ backgroundImage: "url(" + image + ")" }}
             />
-          ) : null} */}
+          ) : null}
         </Drawer>
       </Hidden>
     </div>
