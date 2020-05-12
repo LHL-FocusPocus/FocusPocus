620137d9967d05a43c6542de7ded49061638a633
Taha Rizvi <riztaha@gmail.com>
2020-05-12 00:12:20 -0400

    Added db query to get monthly blacklisted browse time

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 module.exports = (db) => {
   const dbHelper = require("../helpers/dbHelper")(db);
-  const { createDonutData } = require("../helpers/dataProcessor");
+  const {
+    createDonutData,
+    createChartData,
+  } = require("../helpers/dataProcessor");
 
   // Return data needed to render dashboard, access at /data/dashboard
   router.get("/dashboard", (req, res) => {
@@ -21,6 +24,7 @@ module.exports = (db) => {
       dbHelper.getBrowseInfoTodayForDashboard(userId),
       dbHelper.getTotalTimeForTodayByUserID(userId),
       dbHelper.getTotalBlacklistTimeForTodayByUserID(userId),
+      dbHelper.getMonthBlacklistBrowsingInfoForChart(userId),
     ]).then((all) => {
       // all is now an array of data that each promise returns
       userData["user"] = all[0];
@@ -30,68 +34,13 @@ module.exports = (db) => {
       // userData["donut_data"] = all[3];
       userData["total_browse_time_today"] = all[4];
       userData["blacklisted_browse_time_today"] = all[5];
+      userData["month_blacklisted_browse_time_for_chart"] = createChartData(
+        all[6]
+      );
+      // userData["month_browse_time_for_chart"] = all[6];
+
       return res.status(200).json(userData);
     });
-
-    // Getting users id & name
-    // dbHelper
-    //   // user email is hardcoded right now, this will need to be userEmail instead.
-    //   .getUserWithID(userId)
-    //   .then((user) => {
-    //     // res.json(user);
-    //     data["first_name"] = user.first_name;
-    //     data["last_name"] = user.last_name;
-    //     console.log(data);
-    //     return data;
-    //   })
-    //   // Getting users quotas for the day
-    //   .then(() => {
-    //     return dbHelper.getQuotaForTodayWithUserID(userId);
-    //   })
-    //   .then((quota) => {
-    //     data["time_allotment"] = quota.time_allotment;
-    //     data["date_quota_valid_from"] = quota.date_valid_from;
-    //     data["date_quota_valid_until"] = quota.date_valid_until;
-    //     return data;
-    //   })
-    //   // Getting blacklisted sites for user
-    //   .then(() => {
-    //     return dbHelper.getBlacklistedSitesWithUserID(userId);
-    //   })
-    //   .then((blacklists) => {
-    //     data["blacklisted_sites"] = blacklists;
-    //     return data;
-    //   })
-    //   // Getting total browse history recorded for user
-    //   .then(() => {
-    //     return dbHelper.getBrowseInfoWithUserID(userId);
-    //   })
-    //   .then((browse_history) => {
-    //     data["browse_history"] = browse_history;
-    //     return data;
-    //   })
-    //   // Getting total time spent today browsing for user
-    //   .then(() => {
-    //     return dbHelper.getTotalTimeForTodayByUserID(userId);
-    //   })
-    //   .then((total_time) => {
-    //     data["total_browse_time_today"] = total_time;
-    //     return data;
-    //   })
-    //   // Getting time spent today browsing blacklisted sites for user
-    //   .then(() => {
-    //     return dbHelper.getTotalBlacklistTimeForTodayByUserID(
-    //       req.session.userId
-    //     );
-    //   })
-    //   .then((bad_time) => {
-    //     data["blacklisted_browse_time_today"] = bad_time;
-    //     return data;
-    //   })
-    //   .then((data) => {
-    //     res.send(data);
-    //   })
-    //   .catch((err) => res.json(err));
   });
   return router;
 };
