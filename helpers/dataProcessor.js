df0cecd48ce00e439b1bdd2f5ee12b0d7608d860
Taha Rizvi <riztaha@gmail.com>
2020-05-12 14:24:42 -0400

    Refactored create___data to compileData

--- helpers/dataProcessor.js
+++ helpers/dataProcessor.js
@@ -1,37 +1,14 @@
-const createDonutData = function (data) {
+const compileData = function (data, key) {
   const dataArr = [];
-
   data.map((obj) => {
     const dataObj = {};
-    dataObj["website"] = obj.website;
-    processTime(obj, dataObj, dataArr);
+    dataObj[key] = obj[key];
+    processTimeToMins(obj, dataObj, dataArr);
   });
   return dataArr;
 };
 
-const createChartData = function (data) {
-  const dataArr = [];
-
-  data.map((obj) => {
-    const dataObj = {};
-    dataObj["date"] = obj.date;
-    processTime(obj, dataObj, dataArr);
-  });
-  return dataArr;
-};
-
-const createBoardData = function (data) {
-  const dataArr = [];
-
-  data.map((obj) => {
-    const dataObj = {};
-    dataObj["name"] = obj.name;
-    processTime(obj, dataObj, dataArr);
-  });
-  return dataArr;
-};
-
-const processTime = function (obj, dataObj, dataArr) {
+const processTimeToMins = function (obj, dataObj, dataArr) {
   let minutes = 0;
   if (obj.time.days) {
     minutes += obj.time.days * 1440;
@@ -49,8 +26,4 @@ const processTime = function (obj, dataObj, dataArr) {
   dataArr.push(dataObj);
 };
 
-module.exports = {
-  createDonutData,
-  createChartData,
-  createBoardData,
-};
+module.exports = { compileData };
