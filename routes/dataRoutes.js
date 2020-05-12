b0f4ed731121738e52767c5671a04e803cdc99dc
xynyx <matt.taylr22@gmail.com>
2020-05-12 11:22:26 -0400

    Daily quota now returns as % of used allotment

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -7,7 +7,7 @@ module.exports = (db) => {
     createDonutData,
     createChartData,
     createBoardData,
-    toMinutes
+    toMinutes,
   } = require("../helpers/dataProcessor");
 
   // Return data needed to render dashboard, access at /data/dashboard
@@ -32,16 +32,15 @@ module.exports = (db) => {
     ]).then((all) => {
       // all is now an array of data that each promise returns
       userData["user"] = all[0];
-      // console.log(all[1])
-      userData["quota_today"] = (toMinutes(all[1].time_allotment));
-      console.log(toMinutes(all[1].time_allotment))
+      userData["quota_today"] =
+        toMinutes(all[5].sum) / toMinutes(all[1].time_allotment);
+      console.log("allotment", toMinutes(all[1].time_allotment));
+      console.log("used allotment", toMinutes(all[5].sum));
       userData["blacklisted"] = all[2];
       userData["donutGraph"] = createDonutData(all[3]);
       userData["total_browse_time_today"] = all[4];
       userData["blacklisted_browse_time_today"] = all[5];
-      userData["lineGraph"] = createChartData(
-        all[6]
-      );
+      userData["lineGraph"] = createChartData(all[6]);
       userData["leaderboard"] = createBoardData(all[7]);
       userData["shameboard"] = createBoardData(all[8]);
 
