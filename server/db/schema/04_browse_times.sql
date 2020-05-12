86c65d383769172261dc944b92a8b06167010d74
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-12 18:34:24 -0400

    add postgres fn to calculate browse_times start

--- server/db/schema/04_browse_times.sql
+++ server/db/schema/04_browse_times.sql
@@ -7,6 +7,22 @@ CREATE TABLE browse_times
   user_id             INTEGER       REFERENCES users(id) ON DELETE CASCADE,
   website_id          INTEGER       REFERENCES websites(id) ON DELETE CASCADE,
 
-  datetime_start      TIMESTAMP     WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  datetime_start      TIMESTAMP     WITH TIME ZONE,
   duration            INTERVAL      NOT NULL DEFAULT '1 minute'
-)
+);
+
+-- Calculate datetime_start from current_timestamp and duration
+create or replace function insert_browse_time() returns trigger as $$
+begin
+	if NEW.datetime_start is null then
+		NEW.datetime_start := CURRENT_TIMESTAMP - NEW.duration;
+	end if;
+	return new;
+end;
+$$ language plpgsql;
+
+create trigger trig_insert_browse_times
+before insert
+on browse_times
+for each row
+execute procedure insert_browse_time();
