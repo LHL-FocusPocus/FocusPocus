4f9c08d80abdc00f30d42eb80936c8ec306d76f6
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-11 20:08:27 -0400

    implement dom change watcher for videos

--- chrome-extension/changeVideos.js
+++ chrome-extension/changeVideos.js
@@ -9,6 +9,7 @@
   };
 
   const replaceAllVideosOnPage = function (newVideo = newVideoGlobal) {
+    console.log("called");
     const videoTagElements = Array.from(document.querySelectorAll("video"));
 
     // Exclude already-replaced videos from being altered
@@ -21,10 +22,13 @@
     }
   };
 
-  // Wait 5 seconds after page is loaded then start replacing images
+  // Wait 5 seconds after page is loaded then start replacing videos
   ready(() => {
-    setInterval(() => {
+    setTimeout(() => {
       replaceAllVideosOnPage();
+
+      // Set up listener for DOM changes (infinite scroll websites)
+      onNewElementLoaded("body", replaceAllVideosOnPage);
     }, 5000);
   });
 }
