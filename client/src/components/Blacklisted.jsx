088325bf1cd12254768c8a568ff580c2ad41cca2
xynyx <matt.taylr22@gmail.com>
2020-05-13 13:33:30 -0400

    Initial card component for blacklisted sites

--- client/src/components/Blacklisted.jsx
+++ client/src/components/Blacklisted.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import BlacklistedCards from "./BlacklistedCards"
+import styled from "styled-components";
+import Box from "@material-ui/core/Box"
+
+const Container = styled(Box)`
+  height: 80vh;
+  padding: 0 40%;
+`
 
 export default function Blacklisted() {
   return (
-    <div>
-      TEST
-    </div>
+    <Container>
+      <BlacklistedCards />
+    </Container>
   )
 }
