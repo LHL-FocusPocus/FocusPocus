719ef068be299b40277daaecf3104d3c16eb6983
Taha Rizvi <riztaha@gmail.com>
2020-05-12 22:55:07 -0400

    Cleaned up seeds

--- server/db/seeds/05_quotas.sql
+++ server/db/seeds/05_quotas.sql
@@ -1,8 +1,11 @@
 INSERT INTO quotas
   (user_id, time_allotment)
 VALUES
-  (1, '3 hours');
-INSERT INTO quotas
-  (user_id, time_allotment)
-VALUES
-  (2, '4 hours');
+  (1, '3 hours'),
+  (2, '4 hours'),
+  (3, '2 hours'),
+  (4, '2 hours'),
+  (5, '2 hours'),
+  (6, '2 hours'),
+  (7, '2 hours'),
+  (8, '2 hours');
