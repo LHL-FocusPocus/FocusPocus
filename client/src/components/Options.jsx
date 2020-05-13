475fb3dfb4bade286106e93ca48eaeb75c2f8633
xynyx <matt.taylr22@gmail.com>
2020-05-13 11:54:54 -0400

    Initial options page w/ navbar

---
+++ client/src/components/Options.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+export default function Options({ user }) {
+  return (
+    <div>
+      <Navbar user={user} />
+      TEST
+    </div>
+  );
+}
