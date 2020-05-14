cf7229153f626ed6748269c8e1f8bbba17ee9d3f
xynyx <matt.taylr22@gmail.com>
2020-05-14 02:06:47 -0400

    State now updates/renders for delete!

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -18,6 +18,7 @@ export default function useApplicationData() {
     axios
       .get("/api/data/dashboard")
       .then(userData => {
+        console.log('GETTING CALLED')
         const dashboardData = userData.data;
         dispatch({
           type: SET_DASHBOARD_DATA,
--- client/src/reducers/application.js
+++ client/src/reducers/application.js
@@ -15,42 +15,20 @@ export default function reducer(state, action) {
         blacklisted: action.blacklisted,
       };
     case CHANGE_BLACKLIST:
-      // console.log("state", state);
-      // console.log("action", action);
-      // const blacklist = state.blacklist;
-      // console.log("blacklist", blacklist);
-      // console.log("action.id", action.id);
       const { id } = action;
       // Find index where the blacklisted site lives
-      const siteIndex = state.blacklist.indexOf(
-        state.blacklist.find(site => site.blacklists_id === id)
+
+      const siteIndex = state.blacklisted.indexOf(
+        state.blacklisted.find(site => site.blacklists_id === id)
       );
 
-      const clonedBlacklist = [...state.blacklist]
+      const clonedBlacklist = [...state.blacklisted];
 
       clonedBlacklist.splice(siteIndex, 1);
 
-      
-      // console.log("siteIndex", siteIndex);
-      // const siteIndex = blacklist.indexOf(blacklist["blacklist_id"] === action.id)
-      // console.log('siteIndex', siteIndex)
-      // const newBlacklist = {
-      //   ...state.blacklist[siteIndex],
-      // };
-      console.log('state', state)
-
-      // return {
-      //   ...state,
-
-      // }
-
-      // console.log('newBlacklist', newBlacklist)
+      return {
+        ...state,
+        blacklisted: clonedBlacklist,
+      };
   }
 }
-
-/* [] {blacklists_id: 3, user_id: 1, website_id: 3, hostname: "twitter.com", name: "Twitter", …}
-1: {blacklists_id: 4, user_id: 1, website_id: 5, hostname: "cnn.com", name: "CNN", …}
-2: {blacklists_id: 5, user_id: 1, website_id: 6, hostname: "instagram.com", name: "Instagram", …}
-3: {blacklists_id: 6, user_id: 1, website_id: 7, hostname: "pinterest.ca", name: "Pinterest", …}
-4: {blacklists_id: 7, user_id: 1, website_id: 8, hostname: "youtube.com", name: "Youtube", …}
-length: 5 */
