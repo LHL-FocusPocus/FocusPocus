87fcaad769ea052cbb258316e56db73e34149f00
Taha Rizvi <riztaha@gmail.com>
2020-05-14 17:26:37 -0400

    Changed email logins

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -2,11 +2,12 @@ INSERT INTO users
   (first_name, last_name, email, password)
 VALUES
   ('Dwight', 'Schrute', 'a@a.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Michael', 'Scott', 'test@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Jim', 'Halpert', 'Joey@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Pam', 'Beesly', 'Phoebe@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Kevin', 'Malone', 'Monica@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Toby', 'Flenderson', 'Chandler@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Stanley', 'Hudson', 'Ross@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Phyllis', 'Vance', 'Rachel@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'); --password is hash of 'password'
+  ('Michael', 'Scott', 'BigBoss@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
+  ('Jim', 'Halpert', 'Jim@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
+  ('Pam', 'Beesly', 'Pam@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
+  ('Kevin', 'Malone', 'Kevin@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
+  ('Toby', 'Flenderson', 'Toby@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
+  ('Stanley', 'Hudson', 'Stanley@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
+  ('Phyllis', 'Vance', 'Phyllis@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'); --password is hash of 'password'
+
 
