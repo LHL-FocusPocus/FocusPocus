1b282bfcfac99de1a1d61ccc0fb87e38c977a00b
Taha Rizvi <riztaha@gmail.com>
2020-05-13 20:23:03 -0400

    Added criteria to table, only allow 1 user-id_website_id of the same relationship

--- db/schema/03_blacklists.sql
+++ db/schema/03_blacklists.sql
@@ -5,5 +5,6 @@ CREATE TABLE blacklists
   id          SERIAL  PRIMARY KEY,
 
   user_id     INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  website_id  INTEGER NOT NULL REFERENCES websites(id) ON DELETE CASCADE
+  website_id  INTEGER NOT NULL REFERENCES websites(id) ON DELETE CASCADE,
+  UNIQUE      (user_id, website_id) --This only allows 1 user_id-website_id relation to exist in the table
 )
