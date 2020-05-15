7dd2d468c57210f724219ea68587273c37b8e4d1
Taha Rizvi <riztaha@gmail.com>
2020-05-15 12:52:52 -0400

    Can you add to a prop?

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -97,7 +97,7 @@ export default function Navbar(props) {
     .then(res => {
       console.log(res);
       console.log("Successful Logout");
-      history.push("/");
+      props.history.push("/");
     })
     .catch(e => {
       console.error(e);
