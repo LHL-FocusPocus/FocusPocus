0a0c3028f6955f5d8a2aeea2f4444fa281358fd6
xynyx <matt.taylr22@gmail.com>
2020-05-15 01:20:38 -0400

    Get slider to get value with onChange

--- client/src/components/QuotaSlider.jsx
+++ client/src/components/QuotaSlider.jsx
@@ -4,6 +4,8 @@ import Typography from "@material-ui/core/Typography";
 import Slider from "@material-ui/core/Slider";
 import styled from "styled-components";
 import Button from "@material-ui/core/Button";
+import useFormFields from "../hooks/useFormFields";
+import axios from "axios";
 
 const SliderDiv = styled.div`
   width: 400px;
@@ -13,28 +15,72 @@ function valuetext(value) {
   return `${value}°C`;
 }
 
+console.log("valuetext", valuetext);
+
 export default function DiscreteSlider() {
   const [disabled, setDisabled] = useState(true);
+  // const [fields, handleFieldChange] = useFormFields({
+  //   quota: 0,
+  // });
+  const [value, setValue] = React.useState(120);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    console.log("valuetext", valuetext(value));
+  };
+
+  function valuetext(value) {
+    return `${value} minutes/day`;
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log("test");
+
+    // const credentials = {
+    //   email: fields.email,
+    //   password: fields.password,
+    // };
+
+    // axios
+    //   .post("/api/user/adjust_quota", credentials, { withCredentials: true })
+    //   .then((res) => {
+    //     console.log('res', res)
+    //   })
+    //   .catch(e => {
+    //     console.error(e);
+    //   });
+
+    setDisabled(!disabled);
+  };
+
   return (
     <SliderDiv>
       <Typography id="discrete-slider" gutterBottom>
         Daily Quota (Minutes)
       </Typography>
       <Slider
-        defaultValue={30}
-        getAriaValueText={valuetext}
         aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
+        getAriaValueText={valuetext}
         step={10}
         marks
         min={0}
-        max={120}
+        max={180}
         disabled={disabled}
+        onChange={handleChange}
       />
-      <Button onClick={() => setDisabled(!disabled)} variant="contained">
-        {disabled && "Change Quota"} {/* STRETCH: can't change again until: 7 days from now */}
-        {!disabled && "Set Quota"}
-      </Button>
+      {/* STRETCH: can't change again until: 7 days from now */}
+      {disabled && (
+        <Button onClick={() => setDisabled(!disabled)} variant="contained">
+          Change Quota
+        </Button>
+      )}
+      {!disabled && (
+        <Button onClick={handleSubmit} variant="contained">
+          Set New Quota
+        </Button>
+      )}
     </SliderDiv>
   );
 }
