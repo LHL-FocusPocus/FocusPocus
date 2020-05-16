143318c5a6731fc93d3e0f5d614b5f2f8ccfde40
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-16 19:07:59 -0400

    reverse shameboard query for graph

--- helpers/dbHelper.js
+++ helpers/dbHelper.js
@@ -347,7 +347,7 @@ module.exports = (db) => {
         AND datetime_start < CURRENT_DATE + INTERVAL '1 day'
         GROUP BY users.id
         ORDER BY time
-        ASC LIMIT 6;
+        ASC LIMIT 5;
         `
       )
       .then((res) => {
@@ -368,11 +368,11 @@ module.exports = (db) => {
         AND datetime_start < CURRENT_DATE + INTERVAL '1 day'
         GROUP BY users.id
         ORDER BY time
-        DESC LIMIT 6;
+        DESC LIMIT 5;
         `
       )
       .then((res) => {
-        return res.rows;
+        return res.rows.reverse();
       })
       .catch((err) => err);
   };
