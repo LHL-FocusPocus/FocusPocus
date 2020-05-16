791441d9e01ea355ffa02a8f2cb8e4982024331d
xynyx <matt.taylr22@gmail.com>
2020-05-16 11:15:11 -0400

    Remove console.logs

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -22,20 +22,8 @@ export default function useApplicationData() {
     shameboard: [],
     user: {},
   });
-  console.log("state", state);
 
   // const [loading, setLoading] = useState(false)
-
-  const setDashboard = async () => {
-    const userData = await axios.get("/api/data/dashboard");
-    console.log(userData);
-    const dashboardData = userData.data;
-    dispatch({
-      type: SET_DASHBOARD_DATA,
-      payload: dashboardData,
-    });
-  };
-
   useEffect(() => {
     Promise.all([
       axios.get("/api/data/dashboard"),
@@ -54,9 +42,17 @@ export default function useApplicationData() {
       .catch(e => console.error(e));
   }, []);
 
+  const setDashboard = async () => {
+    const userData = await axios.get("/api/data/dashboard");
+    const dashboardData = userData.data;
+    dispatch({
+      type: SET_DASHBOARD_DATA,
+      payload: dashboardData,
+    });
+  };
+
   const disableBlacklistedSite = id => {
     axios.put(`/api/user/blacklists/disable/${id}`, id).then(res => {
-      console.log("TEST");
       dispatch({
         type: CHANGE_BLACKLIST,
         id: res.data.id,
