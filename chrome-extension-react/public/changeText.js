7bd6872f24a3075c6ab527a689f50ba3889032b1
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-16 14:51:42 -0400

    set up scaffolding for changeText

--- chrome-extension-react/public/changeText.js
+++ chrome-extension-react/public/changeText.js
@@ -1,3 +1,19 @@
 /**
  * This script block is injected into the page to replace text.
  */
+{
+  const replaceAllTextOnPage = function () {};
+  
+  
+  ready(() => {
+    setTimeout(() => {
+      replaceAllTextOnPage();
+
+      // Set up listener for DOM changes (infinite scroll websites) and clicks
+      // (instagram like button)
+      addListeners("body", () => {
+        replaceAllTextOnPage();
+      });
+    }, 3000);
+  });
+}
