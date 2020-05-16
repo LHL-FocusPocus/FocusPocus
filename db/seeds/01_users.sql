3f9a6e4b2c3d5b8de5562ae3a542e2916709a323
Taha Rizvi <riztaha@gmail.com>
2020-05-16 16:08:05 -0400

    Added images to table/seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,13 +1,13 @@
 INSERT INTO users
-  (first_name, last_name, email, password)
+  (first_name, last_name, email, password, picture)
 VALUES
-  ('Dwight', 'Schrute', 'a@a.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Michael', 'Scott', 'BigBoss@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Jim', 'Halpert', 'Jim@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Pam', 'Beesly', 'Pam@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Kevin', 'Malone', 'Kevin@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Toby', 'Flenderson', 'Toby@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Stanley', 'Hudson', 'Stanley@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), --password is hash of 'password'
-  ('Phyllis', 'Vance', 'Phyllis@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'); --password is hash of 'password'
+  ('Dwight', 'Schrute', 'a@a.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://pbs.twimg.com/profile_images/1212841876895813632/4mzarqqS_400x400.jpg'), --password is hash of 'password'
+  ('Michael', 'Scott', 'BigBoss@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://i1.sndcdn.com/avatars-000162505694-i81i0k-t500x500.jpg'), --password is hash of 'password'
+  ('Jim', 'Halpert', 'Jim@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://pbs.twimg.com/profile_images/838818903933386752/rVy94Uu6_400x400.jpg'), --password is hash of 'password'
+  ('Pam', 'Beesly', 'Pam@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://i.pinimg.com/originals/f2/1d/ba/f21dbab3929c43a610d276ec88d72009.jpg'), --password is hash of 'password'
+  ('Kevin', 'Malone', 'Kevin@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://vignette.wikia.nocookie.net/theoffice/images/b/b2/2009Kevincropped.PNG/revision/latest?cb=20170701083657'), --password is hash of 'password'
+  ('Toby', 'Flenderson', 'Toby@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://upload.wikimedia.org/wikipedia/en/thumb/1/18/Toby_Flenderson_promo_picture.jpg/220px-Toby_Flenderson_promo_picture.jpg'), --password is hash of 'password'
+  ('Stanley', 'Hudson', 'Stanley@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://media.giphy.com/media/k1AjAKzYEQJtC/giphy.gif'), --password is hash of 'password'
+  ('Phyllis', 'Vance', 'Phyllis@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://img1.looper.com/img/gallery/whatever-happened-to-phyllis-from-the-office/intro-1573062962.jpg'); --password is hash of 'password'
 
 
