554b56bafbdb8b437bf61edab9c89c4203e05569
Taha Rizvi <riztaha@gmail.com>
2020-05-18 03:03:22 -0400

    Added another friend seed.

--- server/db/seeds/06_friends.sql
+++ server/db/seeds/06_friends.sql
@@ -3,5 +3,7 @@ INSERT INTO friends
 VALUES
   (1, 8, false),
   (8, 1, false),
-  (1, 3, true)
+  (1, 9, false),
+  (9, 1, false),
+  (1, 3, true) -- user_id 1 is waiting for friend_id 3 to accept the request.
   ;
