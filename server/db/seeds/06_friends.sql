ea98b0f4f269b6570153821a15d231d863887c92
Taha Rizvi <riztaha@gmail.com>
2020-05-18 03:51:40 -0400

    Added seed for friend notification

--- server/db/seeds/06_friends.sql
+++ server/db/seeds/06_friends.sql
@@ -5,5 +5,6 @@ VALUES
   (8, 1, false),
   (1, 9, false),
   (9, 1, false),
-  (1, 3, true) -- user_id 1 is waiting for friend_id 3 to accept the request.
+  (1, 3, true), -- user_id 1 is waiting for friend_id 3 to accept the request.
+  (5, 1, true) -- user_id 1 will recieve a notification from user 5
   ;
