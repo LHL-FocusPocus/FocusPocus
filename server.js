f793c357c21569184caa412e0ca5af2c3dcef5ee
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-19 15:05:06 -0400

    edit ACAO header for heroku

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ db.connect();
 app.use(morgan("dev"));
 app.use(cors({ credentials: true, preflightContinue: true }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT || "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
