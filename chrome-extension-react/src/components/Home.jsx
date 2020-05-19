ad4a467e706ff8657c406869002a3f7512266b7c
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-19 00:30:26 -0400

    add shadow effect

--- chrome-extension-react/src/components/Home.jsx
+++ chrome-extension-react/src/components/Home.jsx
@@ -61,6 +61,7 @@ const Wrapper = styled(Container)`
   background-color: white;
   border: 1px solid black;
   border-radius: 1em;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
 `;
 
 export default function Home(props) {
