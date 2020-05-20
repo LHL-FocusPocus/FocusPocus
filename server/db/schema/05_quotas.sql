5c5ec4ff0d93ccfea11e4b7865c79b4883746b9f
Taha Rizvi <riztaha@gmail.com>
2020-05-19 22:53:39 -0400

    Changed default in table to 3hrs.

--- server/db/schema/05_quotas.sql
+++ server/db/schema/05_quotas.sql
@@ -5,7 +5,7 @@ CREATE TABLE quotas
   id                SERIAL    PRIMARY KEY,
 
   user_id           INTEGER   REFERENCES users(id) ON DELETE CASCADE,
-  time_allotment    INTERVAL  NOT NULL default '2 hours',
+  time_allotment    INTERVAL  NOT NULL default '3 hours',
 
   date_valid_from   DATE      DEFAULT CURRENT_DATE,
   date_valid_until  DATE      DEFAULT 'INFINITY'
